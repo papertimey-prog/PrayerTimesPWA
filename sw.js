c7315477d5465dfb65bda58825161db31af4ab50
papertimey-prog <papertimey@icloud.com>
2025-12-27 00:51:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-const CACHE_NAME = 'prayer-static-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'salah-pro-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/adhan/3.0.0/Adhan.min.js'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
